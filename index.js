2117c132c65096aff9da608046f73e0a66a095c1
shrawankc11 <np03cs4m210033@heraldcollege.edu.np>
2022-10-02 10:13:58 +0545

    main server and socket file added

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+const users = {};
+
+app.get('/', (_req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    socket.on('new-user', (name) => {
+        users[socket.id] = name;
+        socket.broadcast.emit('user-connected', name);
+    });
+
+    socket.on('chat message', (msg) => {
+        socket.broadcast.emit('chat message', { msg, name: users[socket.id] });
+    });
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('user-disconnected', { name: users[socket.id] });
+        delete users[socket.id];
+    });
+
+    socket.on('user-typing', () => {
+        socket.broadcast.emit('user-typing', { name: users[socket.id] });
+    });
+
+    socket.on('user-not-typing', () => {
+        socket.broadcast.emit('user-not-typing', 'K');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
